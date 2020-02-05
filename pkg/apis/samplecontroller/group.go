781e9bee986696995aedb0fcba5bcc24f19c63d0
Kenta Iso <type.mafty@gmail.com>
2020-02-06 00:00:47 +0900

    Create API Resource by following command 'operator-sdk add api --api-version=samplecontroller.k8s.io/v1alpha1 --kind=Foo'

---
+++ pkg/apis/samplecontroller/group.go
@@ -0,0 +1,6 @@
+// Package samplecontroller contains samplecontroller API versions.
+//
+// This file ensures Go source parsers acknowledge the samplecontroller package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package samplecontroller
---
+++ pkg/apis/samplecontroller/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the samplecontroller v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=samplecontroller.k8s.io
+package v1alpha1
